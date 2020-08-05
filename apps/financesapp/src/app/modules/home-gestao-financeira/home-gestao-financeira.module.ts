c23f3159efb597bb6131b76d2df8d5dbe33e4bdc
danvieira <danemmanuelturibio@gmail.com>
2020-08-05 18:25:30 -0300

    remover componentes nao utilizados

--- apps/financesapp/src/app/modules/home-gestao-financeira/home-gestao-financeira.module.ts
+++ apps/financesapp/src/app/modules/home-gestao-financeira/home-gestao-financeira.module.ts
@@ -3,9 +3,6 @@ import { CommonModule } from '@angular/common';
 import { HomeGestaoFinanceiraComponent } from './home-gestao-financeira.component';
 import { HomeGestaoFinanceiraRoutingModule } from './home-gestao-financeira-routing.module';
 import { AngularMaterialModule } from '@finances-app/angular-material';
-import { SaldoEmContasComponent } from './components/saldo-em-contas/saldo-em-contas.component';
-import { SaldoEmReceitasComponent } from './components/saldo-em-receitas/saldo-em-receitas.component';
-import { SaldoEmDespesasComponent } from './components/saldo-em-despesas/saldo-em-despesas.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ChartModule } from 'angular-highcharts';
 import { DespesasPendentesComponent } from './components/despesas-pendentes/despesas-pendentes.component';
@@ -16,9 +13,6 @@ import { FitCardModule } from '@finances-app/fit-card';
 @NgModule({
   declarations: [
     HomeGestaoFinanceiraComponent,
-    SaldoEmContasComponent,
-    SaldoEmReceitasComponent,
-    SaldoEmDespesasComponent,
     DespesasPendentesComponent,
     ReceitasPendentesComponent,
     IndicativosFinanceirosComponent,
