a5b907b352bb941f0126e984e92bea2bd6f0b523
danvieira <danemmanuelturibio@gmail.com>
2020-08-05 18:41:49 -0300

    melhorias no fit card

--- apps/financesapp/src/app/modules/receitas/receitas.module.ts
+++ apps/financesapp/src/app/modules/receitas/receitas.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReceitasComponent } from './receitas.component';
 import { ReceitasRoutingModule } from '@finances-app/src/app/modules/receitas/receitas-routing.module';
+import { FitCardModule } from '@finances-app/fit-card';
 
 @NgModule({
   declarations: [ReceitasComponent],
-  imports: [CommonModule, ReceitasRoutingModule]
+  imports: [CommonModule, ReceitasRoutingModule, FitCardModule]
 })
 export class ReceitasModule {
 }
