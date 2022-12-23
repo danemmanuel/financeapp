865a1333de47575a0a0a0775c21740203c4a95c7
Dan Vieira <dan.vieira@gps-pamcary.com.br>
2022-12-23 08:28:18 -0300

    voltar ao mes atual

--- libs/header-mes/src/lib/header-mes/header-mes.component.ts
+++ libs/header-mes/src/lib/header-mes/header-mes.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { HeaderMesAnoService } from '@finances-app-libs/header-mes/src/lib/header-mes/header-mes-ano.service';
-import {MatIconModule} from "@angular/material/icon";
-import {MatButtonModule} from "@angular/material/button";
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import {NgIf} from "@angular/common";
+import {MatTooltipModule} from "@angular/material/tooltip";
 @Component({
   selector: 'finances-app-header-mes',
   templateUrl: './header-mes.component.html',
   styleUrls: ['./header-mes.component.scss'],
   standalone: true,
-  imports: [MatIconModule, MatButtonModule],
+  imports: [MatIconModule, MatButtonModule, NgIf, MatTooltipModule],
 })
 export class HeaderMesComponent implements OnInit {
   data = new Date();
@@ -57,5 +59,21 @@ export class HeaderMesComponent implements OnInit {
       mes: this.data.getMonth() + 1,
       ano: this.data.getFullYear(),
     });
+  }
+
+  checkMes(): boolean {
+    const mesAtual = new Date().getMonth();
+    if (this.meses[mesAtual] !== this.mesAtual) {
+      return true;
+    }
+  }
+
+  setMesAtual() {
+    const dataAtual = new Date();
+    this.data = dataAtual;
+    this.data.setMonth(dataAtual.getMonth());
+    this.mesAtual = this.meses[dataAtual.getMonth()];
+    this.anoAtual = dataAtual.getFullYear();
+    this.atualizarMesAno();
   }
 }
