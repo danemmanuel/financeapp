6a63bdfec23fee33c7884d0c13b1b65dcaf503ae
Dan Vieira <dan.vieira@gps-pamcary.com.br>
2022-12-23 14:21:14 -0300

    melhoria menu mobile

--- libs/main-menu/src/lib/main-menu/main-menu.component.ts
+++ libs/main-menu/src/lib/main-menu/main-menu.component.ts
@@ -2,17 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { AngularMaterialModule } from '@finances-app/angular-material';
 import { RouterModule } from '@angular/router';
 import { AuthService } from '@finances-app/src/app/core/auth/auth.service';
-import {JsonPipe} from "@angular/common";
+import {JsonPipe, NgClass, NgIf} from "@angular/common";
 
 @Component({
   selector: 'finances-app-main-menu',
   templateUrl: './main-menu.component.html',
   styleUrls: ['./main-menu.component.scss'],
-  imports: [AngularMaterialModule, RouterModule, JsonPipe],
+  imports: [AngularMaterialModule, RouterModule, JsonPipe, NgClass, NgIf],
   standalone: true,
 })
 export class MainMenuComponent implements OnInit {
   dadosUsuario;
+  menuOpened = false;
+  isMobile = window.innerWidth < 768;
 
   constructor(private authService: AuthService) {
     this.authService.getDadosUsuario().subscribe((r) => {
@@ -23,6 +25,9 @@ export class MainMenuComponent implements OnInit {
 
   ngOnInit(): void {}
 
+  openMenu() {
+    this.menuOpened = !this.menuOpened;
+  }
   logout() {
     localStorage.removeItem('token');
     window.location.reload();
